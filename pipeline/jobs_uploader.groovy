79f5b8d841006874e97f492b06c4d90f760ce8be
Belozerov Vladimir <telemetristika@gmail.com>
2026-09-07 20:43:44 +0300

    Use jenkins-jobs from PATH

--- pipeline/jobs_uploader.groovy
+++ pipeline/jobs_uploader.groovy
@@ -27,6 +27,6 @@ node {
     }
 
     stage('Deploy jobs to Jenkins') {
-        sh "/var/jenkins_home/jjb-venv/bin/jenkins-jobs --conf ${CONF_FILE} --flush-cache update ${JOBS_DIR}"
+        sh "jenkins-jobs --conf ${CONF_FILE} --flush-cache update ${JOBS_DIR}"
     }
 }
